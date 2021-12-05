3fea4f086556ddd21a967da818187cc650dd9cb0
SlieFamily <sliewdyinwhite@gmail.com>
2021-12-05 11:19:45 +0100

    README,database,etc.

--- src/plugins/erika.py
+++ src/plugins/erika.py
@@ -16,7 +16,7 @@ sarcasm = on_command("嘲讽",priority=3)
 circle = on_command("专属时间",priority=5)
 welcom = on_notice(priority=4)
 suanle = on_regex("算了",priority=5)
-ping = on_command("ping 胶布",priority=5)
+ping = on_command("QQ通行证",priority=5)
 pick = on_notice(priority=5)
 
 
@@ -64,7 +64,17 @@ async def handle_first_receive(bot: Bot,event: Event, state: T_State):
 
 @ping.handle()
 async def handle(bot: Bot, event: Event, state: T_State):
-    await ping.send(Message("正在 Ping github.com/ErikaBot [220.181.38.148] 具有 32 字节的数据:"))
-    await ping.send(Message("220.181.38.148 的 Ping 统计信息:\n数据包: 已发送 = 4，已接收 = 4，丢失 = 0 (0% 丢失)"))
-    await ping.send(Message("往返行程的估计时间(以毫秒为单位):\n最短 = 31ms，最长 = 32ms，平均 = 31ms"))
-    await ping.finish()
+    # await ping.send(Message("正在 Ping github.com/ErikaBot [220.181.38.148] 具有 32 字节的数据:"))
+    # await ping.send(Message("220.181.38.148 的 Ping 统计信息:\n数据包: 已发送 = 4，已接收 = 4，丢失 = 0 (0% 丢失)"))
+    # await ping.send(Message("往返行程的估计时间(以毫秒为单位):\n最短 = 31ms，最长 = 32ms，平均 = 31ms"))
+    msg = [{'type':'xml','data':{}}]
+    
+    #data = '<?xml version="1.0" encoding="UTF-8" standalone="yes"?><msg serviceID="1"><item><title>[QQ频道]命运石之门 — STEINS;GATE </title></item><source name="频道通行证(破解版)" icon="https://qzs.qq.com/ac/qzone_v5/client/auth_icon.png" action="" appid="-1" /></msg>'
+    
+    data = '''<?xml version='1.0' encoding='UTF-8' standalone='yes' ?><msg serviceID="1" templateID="2" action="web" brief="[QQ频道]通行证" sourceMsgId="2" url="https://qun.qq.com/qqweb/qunpro/share?_wv=3&_wwv=128&inviteCode=H094H&from=246610&biz=ka" flag="3" adverSign="0" multiMsgFlag="0"><item layout="5" advertiser_id="0" aid="0"><picture cover="https://gchat.qpic.cn/gchatpic_new/1269416542/729901771-3068461129-D39946C4CA84A50A7FE9E5B688AB52E8/0?term=2" w="0" h="0" /></item><item layout="6" bg="-1" advertiser_id="0" aid="0"><summary size="27" style="1">☞点击这里☜
+
+进入石头门QQ频道~ ٩(๑ᵒ̴̶̷͈᷄ᗨᵒ̴̶̷͈᷅)و</summary></item><source name="" icon="" action="" appid="-1" /></msg>'''
+
+    msg[0]['data']['data'] = data
+
+    await ping.finish(Message(msg))
