47a809cbce0028c9ec1cf46b368487429f4ea16c
SlieFamily <sliewdyinwhite@gmail.com>
2021-12-06 09:47:10 +0100

    更改正则、帮助

--- src/plugins/erika.py
+++ src/plugins/erika.py
@@ -28,7 +28,7 @@ async def handle(bot: Bot, event: Event, state: T_State):
 
 @sarcasm.got("msg", prompt="？")
 async def got_msg(bot: Bot,event: Event, state: T_State):
-    msg = re.findall("([\u4E00-\u9FA5A-Za-z0-9_]+)，([\u4E00-\u9FA5A-Za-z0-9_]+)",str(state["msg"]))[0]
+    msg = re.findall("([\w\W]+)，([\w\W]+)",str(state["msg"]))[0]
     # print(msg)
     send_msg = f"仅凭借{msg[0]}，古户绘梨花便能{msg[1]}到这种程度，如何呀，诸位~"
     await sarcasm.finish(Message(send_msg))
@@ -47,7 +47,7 @@ async def handle(bot: Bot, event: GroupIncreaseNoticeEvent, state: T_State):
     user = event.get_user_id()
     at_ = "[CQ:at,qq={}]".format(user)
     # msg = at_+'新人，想学爆点吗？'
-    msg = at_+'欢迎新人进裙~\n'
+    msg = at_+'欢迎新人进裙~'
     await welcom.finish(Message(msg))
 
 @pick.handle()
@@ -77,23 +77,27 @@ async def handle(bot: Bot, event: Event, state: T_State):
 @helper.handle()
 async def handle_first_receive(bot: Bot, event: Event, state: T_State):
     help_msg = [{'type':'xml','data':{}}]
-    data = '''<?xml version='1.0' encoding='UTF-8' standalone='yes' ?><msg serviceID="1" templateID="-1" action="" brief="绘梨花|ErikaBot" sourceMsgId="0" url="" flag="3" adverSign="0" multiMsgFlag="0"><item layout="2" mode="1" bg="-18751" advertiser_id="0" aid="0"><picture cover="https://camo.githubusercontent.com/5f0a5f736a2b13cbc49007b26e25776d30880e31b68c4c1c2a98a3a6173b5717/68747470733a2f2f692e6c6f6c692e6e65742f323032312f31312f32382f4173556d656f5379717a6a4e435a722e706e67" w="0" h="0" /><title>绘梨花Bot</title><summary>帮助菜单</summary></item><item layout="6" advertiser_id="0" aid="0"><summary size="28" color="#ff69b4">[嘲讽]：嘲讽 A，B
+    data = '''<?xml version='1.0' encoding='UTF-8' standalone='yes' ?><msg serviceID="1" templateID="-1" action="" brief="绘梨花|ErikaBot" sourceMsgId="0" url="" flag="3" adverSign="0" multiMsgFlag="0"><item layout="2" mode="1" bg="-18751" advertiser_id="0" aid="0"><picture cover="https://gchat.qpic.cn/gchatpic_new/1364374624/3808573830-2554273412-B8EDB73485FB0C183EBB095ECF4BAE54/0?term=3" w="0" h="0" /><title>绘梨花Bot</title><summary>帮助菜单</summary></item><item layout="6" advertiser_id="0" aid="0"><summary size="28" color="#ff69b4">
+[嘲讽]：发送'嘲讽 aaaa，bbbb'
 
-[互动]：请@我，或者戳一戳我
+[互动]：请@，或者戳一戳
 
-[美图]：发送'涩图'、'来点涩图'等
+[美图]：
+    发送'涩图'、'来点涩图'等
         (回复'不够色'有惊喜)
 
-[奥利奥]：发送'来点 奥利*****'
+[奥利奥]：发送'来点 奥利'
 
-[语录]： -添加[add xx语录：zzzzzz]
-        -查找[find：zzzzz](支持模糊查找)
-        -删除[del xx语录：zzzzzz]
+[语录]：   
+    -添加[add xx语录：zzzzzz]
+    -查找[find：zzzzz](支持模糊查找)
+    -删除[del xx语录：zzzzzz]
 
 [倒计时]：每天8点自动发送
 
 [点歌]：发送'点歌 xxx'
 
-[推特]：发送'twitter帮助'查看更多</summary><hr hidden="false" style="0" /></item><source name="by-Slie" icon="http://t.cn/RVIeaZK" url="http://104a.cn" action="" appid="0" /></msg>'''
+[推特]：
+    发送'twitter帮助'查看更多</summary><hr hidden="false" style="0" /></item><source name="by-Slie" icon="http://t.cn/RVIeaZK" url="http://github.com/sliefamily/erikabot" action="" appid="0" /></msg>'''
     help_msg[0]['data']['data'] = data
     await helper.finish(Message(help_msg))
