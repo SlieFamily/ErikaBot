888394eef2d5f9f909c7e1aebb1906a31be4a634
SlieFamily <sliewdyinwhite@gmail.com>
2021-12-26 15:02:55 +0100

    点歌测试

--- src/plugins/erika.py
+++ src/plugins/erika.py
@@ -69,10 +69,16 @@ async def got_msg(bot: Bot,event: Event, state: T_State):
     msg = re.findall("17人",str(state["msg"]))[0]
     print(msg)
     if msg:
-        send_msg = [{'type':'json','data':{}}]
-        data = '''{"app":"com.tencent.structmsg","desc":"音乐","view":"music","ver":"0.0.0.1","prompt":"[分享]birth of a new witch","appID":"","sourceName":"","actionData":"","actionData_A":"","sourceUrl":"","meta":{"music":{"action":"","android_pkg_name":"","app_type":1,"appid":100495085,"ctime":1640524762,"desc":"M.Graveyard","jumpUrl":"https:\/\/y.music.163.com\/m\/song\/786015","musicUrl":"http:\/\/music.163.com\/song\/media\/outer\/url?id=786015","preview":"http:\/\/p2.music.126.net\/GYmSqgsY-zRU8tTFr7C-2Q==\/792747883626755.jpg","sourceMsgId":"0","source_icon":"","source_url":"","tag":"网易云音乐","title":"birth of a new witch","uin":2450509502}},"config":{"autosize":true,"ctime":1640524762,"forward":true,"token":"6a80c60781e34fe213283e1e71ec3ecd","type":"normal"},"text":"","extra":"{\"app_type\":1,\"appid\":100495085,\"msg_seq\":2639423547603752953,\"uin\":2450509502}"}'''
-        send_msg[0]['data']['data'] = data
-        await selfIntro.finish(Message(send_msg))
+        songContent = [
+            {
+                "type": "music",
+                "data": {
+                    "type": 163,
+                    "id": 786015
+                }
+            }
+        ]
+        await selfIntro.finish(Message(songContent))
     else:
         await selfIntro.finish()
 
