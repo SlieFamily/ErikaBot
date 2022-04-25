4aac9a7a38c051105ef59d3b1adabdfb0e164be1
SlieFamily <sliewdyinwhite@gmail.com>
2022-04-25 14:56:12 +0800

    推特又寄了？

--- src/plugins/twitter_helper/__init__.py
+++ src/plugins/twitter_helper/__init__.py
@@ -60,7 +60,7 @@ def flush_token():
 scheduler = require('nonebot_plugin_apscheduler').scheduler
 
 # 创建定时任务：刷新token/每5min更新一次 
-@scheduler.scheduled_job('interval',minutes=5,id='flush_token',timezone='Asia/Shanghai')
+@scheduler.scheduled_job('interval',minutes=1,id='flush_token',timezone='Asia/Shanghai')
 async def flush():
     flush = threading.Thread(target=flush_token)
     flush.start()
